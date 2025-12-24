74c3f3c9e28c056fb408badc8b36bc7524b29865
josh098741 <morongemokaya@gmail.com>
2025-12-24 22:39:56 +0300

    Addition of delete controller route

--- backend/server.js
+++ backend/server.js
@@ -64,7 +64,16 @@ app.post("/api/transactions", async (req,res) => {
 
 app.delete("/api/transactions/:id", async (req,res) => {
     try{
+        const { id } = req.params
 
+        const result = await sql`
+            DELETE FROM transactions WHERE id = ${id} RETURNING * 
+        `
+        if(result.length === 0){
+            return res.status(404).json({ message: "Transaction not found" })
+        }
+
+        res.status(200).json({ message: "Transaction deleted successfully" })
     }catch(error){
         console.log("Error in deleting transaction", error)
         res.status(500).json({ message: "Internal server error in delete transaction controller" })
